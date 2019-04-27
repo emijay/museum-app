d2eae1f987148d41ecd511c874bb42ff95d25ae5
Muhammad Hilmi Bin Johari <hilmijohari@Muhammads-MacBook-Pro.local>
2019-04-27 14:39:38 -0400

    Updates

---
+++ precache-manifest.e2743786b7bc20451d95a5e3e60b062f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d571a453bb1993f20aee61397e9a50a7",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "dbfde47a31579a60ca8c",
+    "url": "/museum-app/static/css/main.93559504.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum-app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "dbfde47a31579a60ca8c",
+    "url": "/museum-app/static/js/main.55655ae3.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
